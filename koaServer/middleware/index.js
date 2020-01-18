e6dc400aedb8583746f1ea15241df6a97f81e75c
tiannengyu <aneng@pinduoduo.com>
2020-01-18 16:08:00 +0800

    fix: :fix travis

--- koaServer/middleware/index.js
+++
@@ -1,8 +0,0 @@
-const logger = async (ctx, next) => {
-    console.log(ctx);
-    console.log(111);
-    console.log(`${Date.now()} ${ctx.request.method} ${ctx.request.url}`);
-    await next();
-};
-
-module.exports = logger;
